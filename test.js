d3241ae65a2d1513a6ce2aa5d1ea506abf5b1fc1
nuva <kayam.moon@gmail.com>
2025-03-16 23:38:41 +0300

    uwu

--- test.js
+++ test.js
@@ -1,5 +1,19 @@
-const button = document.getElementById ("space-btn");
-button.addEventListener("click", myfunction)
-function myfunction () {
-    document.location = "https://x.com/nuvamoon"
+const bubbleText = document.querySelector('#bubble-text');
+bubbleText.textContent = "* "; //we initialize the text of the bubble with an asterisk
+
+const text = "hello world!";
+
+
+
+//this function adds a character to the bubble text
+function addCharacterToBubble(character) {
+    bubbleText.textContent += character; //add the character to the bubble text
 }
+Array.from(text).forEach(
+    (character, index) => {
+        setTimeout(
+            () => addCharacterToBubble(character),
+            80 * index //delay of 1000ms
+        )
+    }
+);
---
+++ test1.js
@@ -0,0 +1,5 @@
+const button = document.getElementById ("space-btn");
+button.addEventListener("click", myfunction)
+function myfunction () {
+    document.location = "https://x.com/nuvamoon"
+}
